92849adb8efef41e1d32a498a05c02d7fa1543c1
JuanCarlosGil <91684824+JuanCarlosGil@users.noreply.github.com>
2021-10-01 15:53:53 -0500

    Add files via upload
    It may be an initial prototype, please take easy, you mightn't have released such a strange idea.

---
+++ index.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <title>MyLittle</title>
+  <link rel="stylesheet" href="css/todos.css" type="text/css">
+  <link rel="stylesheet" href="css/header.css" type="text/css">
+  <link rel="stylesheet" href="css/footer.css" type="text/css">
+  <link rel="stylesheet" href="css/main1.css" type="text/css">
+  <link rel="stylesheet" href="css/main2.css" type="text/css">
+  <link rel="stylesheet" href="css/main3.css" type="text/css">
+</head>
+<body>
+
+    <header>
+        <nav class="desktop">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li id="logo" class="logo"><a href="">Farm's Logo</a></li>
+                <li><a href="">Location</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+
+        <nav class="tablet">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Location</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+        <br>
+        <nav class="tablet">
+            <ul>
+                <li id="logo" class="logo"><a href="">Farm's Logo</a></li>
+            </ul>
+        </nav>
+
+        <h1>Titulo</h1>
+    </header>
+
+    <section class="principal1">
+    <div>
+        <figure>
+            <img src="imagenes/DSCF7241.jpg" alt="This is such a nice goat">
+            <br>
+            <figcaption>I wish I could have one like that some day</figcaption>
+        </figure>
+    </div>
+    <div>
+        <figure>
+            <img src="imagenes/strawberries.webp" alt="I am gonna steal the basket">
+            <br>
+            <figcation>I am gonna steal the basket</figcation>
+        </figure>
+    </div>
+    <div>
+        <figure>
+            <img id="farm" src="imagenes/Upward-Farms-Logo.png" alt="This is a deprecated logo">
+            <br>
+            <figcation>This is a deprecated logo</figcation>
+        </figure>
+    </div>
+    </section>
+
+    <section class="principal2">
+        <div>Principal 2.1</div>
+    </section>
+
+    <section class="principal3">
+        <div id="izq">Principal 3.1</div>
+        <div id="cen">Principal 3.2</div>
+        <div id="der">
+        <div>3.3.1</div>
+        <div>3.3.2</div>
+        <div><aside>This is the best way  to learn speaking English.</aside></div>
+        </div>
+    </section>
+
+    <footer>
+        <div>
+            I live at Chicago street. Av. 101 Boulevard Tomacosi Bulging. Third Floor. Door 306.
+        </div>
+    </footer>
+
+    <div class="image-check">
+        <img src="imagenes/qr.png" alt="imagenes/DSCF7241.jpg">
+    </div>
+
+</body>
+</html>
